ed23ba5ec851963a06a5edbe570cf4fcce9ae825
RoKo-reuther <blubberbob22222@gmail.com>
2021-08-20 17:11:15 +0200

    nothing real ..

--- 01_parameters_config.R
+++ 01_parameters_config.R
@@ -11,14 +11,14 @@
   # porosity: set por.0 and por.inf equal for constant porosity
   por_shape     <- 1                # to shape your porosity profile (used as x.att in p.exp (cf. "03_grid_setup.R"))
   por.0         <- 0.8              # porosity at surface [-] =soil moisture content (at full saturation)
-  por.inf       <- 0.5              # porosity at infinite depth [-]
+  por.inf       <- 0.8              # porosity at infinite depth [-]
   
   TC              <- 10             # Temperature Celsius
   P               <- 0.30           # True pressure [bar] 
   dens_dw         <- 2.70           # Density dry sediment [g cm-3]= [kg L-1] = [10^3kg m-3] 
   S               <- 6.25* 10^-5    # Salinity for fresh water in psu (ppt); K: 430uS/cm -> 0.208 ppt?
   v               <- 10*1e-3        # Sedimentation rate: advection at top [m y-1]
-  Db              <- 1e-4           #  Mixing rate of the sediment; Bioturbation coefficient [m2 y-1]
+  Db              <- 1e-4           # Mixing rate of the sediment; Bioturbation coefficient [m2 y-1]
   
 # stoichiometric relationships
   CtoN            <- 106/16          # C:N ratio after Redfield
