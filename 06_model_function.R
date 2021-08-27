19a019fe3917e0c00720818b9ae7fba365c41751
RoKo-reuther <blubberbob22222@gmail.com>
2021-08-27 16:05:47 +0200

    nothing really ...

--- 06_model_function.R
+++ 06_model_function.R
@@ -10,7 +10,7 @@ Model <- function(t, state, pars) {
   for (i in seq_along(species_operational)){
     assign(names(species_operational[i]), state[((i-1)*N+1):(i*N)])
   }
-
+  
 
   ## Define boundary conditions
     # assign constant values
@@ -21,7 +21,7 @@ Model <- function(t, state, pars) {
   for (i in seq_along(boundary_conditions$varying)){
     assign(names(boundary_conditions$varying[i]), boundary_conditions$varying[[i]](t))
   }
-
+  
   
   ## get current temperature
   TC <- TC_func(t)
@@ -35,7 +35,7 @@ Model <- function(t, state, pars) {
   for (i in seq_along(transport_terms)){
     assign(names(transport_terms[i]), eval(parse(text=transport_terms[[i]]))) 
   }
-  
+
   
   ## Define the reaction terms
     # assign reaction rate constants
@@ -46,8 +46,10 @@ Model <- function(t, state, pars) {
     # assign shared regulation terms
     for (i in seq_along(shared_reg_terms)){
       assign(names(shared_reg_terms[i]), eval(parse(text=shared_reg_terms[[i]])))
+      #print(names(shared_reg_terms[i]))
+      #print(eval(parse(text=shared_reg_terms[[i]])))
     }
-    
+
     # assign rate equations
     for (i in seq_along(rate_equations)){
       assign(names(rate_equations[i]), eval(parse(text=rate_equations[[i]]))) 
@@ -63,7 +65,6 @@ Model <- function(t, state, pars) {
   for (i in seq_along(total_c_change)){
     assign(names(total_c_change[i]), eval(parse(text=total_c_change[[i]]))) 
   }
-  
   
   ## return statement
   return(eval(parse(text = returnlist)))
